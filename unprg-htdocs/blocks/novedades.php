a83c420ba4156e328c50fd90168e5fd2edd0c5e1
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2016-04-01 20:40:01 -0500

    Novedades agregafo

---
+++ unprg-htdocs/blocks/novedades.php
@@ -0,0 +1,197 @@
+<section class="block plx bklast bgc--gris6 cc--gris1">
+	<div class="block__wraper">
+
+		<div class="bklast__col bgc bklast__avi plx__item">
+			<div class="bklast__col__titulo ff--26 ff--special cc--azul2">Avisos</div>
+			<div class="bklast__col__cont">
+
+				<div class="bklast__avi__el">
+					<a class="bklast__avi__el__text ff--14 ff--j" href="#">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae sed harum nulla asperiores?
+					</a>
+					<div class="bklast__avi__el__fech ff--12 ff--r ff--b cc--azul2">
+						@01/04/2016
+					</div>
+				</div>
+
+				<div class="bklast__avi__el">
+					<a class="bklast__avi__el__text ff--14 ff--j" href="#">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus tenetur, aliquid totam possimus perspiciatis molestiae voluptates mollitia sequi aut rerum numquam inventore maxime dolores, tempore. Nihil deleniti eos rerum id.
+					</a>
+					<div class="bklast__avi__el__fech ff--12 ff--r ff--b cc--azul2">
+						@01/04/2016
+					</div>
+				</div>
+
+				<div class="bklast__avi__el">
+					<a class="bklast__avi__el__text ff--14 ff--j" href="#">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</a>
+					<div class="bklast__avi__el__fech ff--12 ff--r ff--b cc--azul2">
+						@01/04/2016
+					</div>
+				</div>
+
+				<div class="bklast__avi__el">
+					<a class="bklast__avi__el__text ff--14 ff--j" href="#">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique enim consequatur asperiores, tempore autem ea assumenda.
+					</a>
+					<div class="bklast__avi__el__fech ff--12 ff--r ff--b cc--azul2">
+						@01/04/2016
+					</div>
+				</div>
+
+				<div class="bklast__avi__el">
+					<a class="bklast__avi__el__text ff--14 ff--j" href="#">
+						Lorem ipsum dolor sit amet, consectetur.
+					</a>
+					<div class="bklast__avi__el__fech ff--12 ff--r ff--b cc--azul2">
+						@01/04/2016
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+		<div class="bklast__col bgc bklast__doc plx__item">
+			<div class="bklast__col__titulo ff--26 ff--special cc--azul2">Documentos</div>
+			<div class="bklast__col__cont">
+
+				<div class="bklast__doc__el">
+					<span class="icon-file-pdf ff--44 cc--gris2"></span>
+					<a class="bklast__doc__el__text ff--14 ff--j" href="#">
+						Lorem ipsum dolor sit amet, consectetur adipisicing.
+					</a>
+					<div class="bklast__doc__el__fech ff--12 ff--r ff--b cc--azul3">
+						@01/04/2016
+					</div>
+				</div>
+
+				<div class="bklast__doc__el">
+					<span class="icon-file-pdf ff--44 cc--gris2"></span>
+					<a class="bklast__doc__el__text ff--14 ff--j" href="#">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt laboriosam, quibusdam obcaecati expedita.
+					</a>
+					<div class="bklast__doc__el__fech ff--12 ff--r ff--b cc--azul3">
+						@01/04/2016
+					</div>
+				</div>
+
+				<div class="bklast__doc__el">
+					<span class="icon-file-pdf ff--44 cc--gris2"></span>
+					<a class="bklast__doc__el__text ff--14 ff--j" href="#">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus esse dolorum, veniam accusamus eius, at.
+					</a>
+					<div class="bklast__doc__el__fech ff--12 ff--r ff--b cc--azul3">
+						@01/04/2016
+					</div>
+				</div>
+
+				<div class="bklast__doc__el">
+					<span class="icon-file-pdf ff--44 cc--gris2"></span>
+					<a class="bklast__doc__el__text ff--14 ff--j" href="#">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur ratione accusamus, illo facere saepe a totam sunt aperiam possimus! Iste quisquam illum cupiditate labore facilis, nihil iure aliquid provident voluptatum!
+					</a>
+					<div class="bklast__doc__el__fech ff--12 ff--r ff--b cc--azul3">
+						@01/04/2016
+					</div>
+				</div>
+
+				<div class="bklast__doc__el">
+					<span class="icon-file-pdf ff--44 cc--gris2"></span>
+					<a class="bklast__doc__el__text ff--14 ff--j" href="#">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</a>
+					<div class="bklast__doc__el__fech ff--12 ff--r ff--b cc--azul3">
+						@01/04/2016
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+		<div class="bklast__col bgc bklast__age plx__item">
+			<div class="bklast__col__titulo ff--26 ff--special cc--azul2">Agenda</div>
+			<div class="bklast__col__cont">
+				
+				<div class="bklast__age__el">
+					<div class="bklast__age__el__fech bgc--azul2 cc ff ff--c">
+						<div class="bklast__age__el__fech__dia ff--special ff--22">02</div>
+						<div class="bklast__age__el__fech__mes ff--special ff--18 cc--amarillo2 ff--r">Abr</div>
+						<div class="bklast__age__el__fech__hor ff--b ff--18">01:30</div>
+					</div>
+					<a class="bklast__age__el__text ff--16 ff--special" href="#">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</a>
+					<div class="bklast__age__el__place ff--14 ff--r ff--b cc--azul3">
+						Biblioteca central de la UNPRG
+					</div>
+					<div class="block__clean"></div>
+				</div>
+
+				<div class="bklast__age__el">
+					<div class="bklast__age__el__fech bgc--azul2 cc ff ff--c">
+						<div class="bklast__age__el__fech__dia ff--special ff--22">05</div>
+						<div class="bklast__age__el__fech__mes ff--special ff--18 cc--amarillo2 ff--r">Abr</div>
+						<div class="bklast__age__el__fech__hor ff--b ff--18">09:00</div>
+					</div>
+					<a class="bklast__age__el__text ff--16 ff--special" href="#">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</a>
+					<div class="bklast__age__el__place ff--14 ff--r ff--b cc--azul3">
+						Centro Franciso Aguinaga Castro
+					</div>
+					<div class="block__clean"></div>
+				</div>
+
+				<div class="bklast__age__el">
+					<div class="bklast__age__el__fech bgc--azul2 cc ff ff--c">
+						<div class="bklast__age__el__fech__dia ff--special ff--22">06</div>
+						<div class="bklast__age__el__fech__mes ff--special ff--18 cc--amarillo2 ff--r">Abr</div>
+						<div class="bklast__age__el__fech__hor ff--b ff--18">16:30</div>
+					</div>
+					<a class="bklast__age__el__text ff--16 ff--special" href="#">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</a>
+					<div class="bklast__age__el__place ff--14 ff--r ff--b cc--azul3">
+						Centro deportivo La Pichanga
+					</div>
+					<div class="block__clean"></div>
+				</div>
+
+				<div class="bklast__age__el">
+					<div class="bklast__age__el__fech bgc--azul2 cc ff ff--c">
+						<div class="bklast__age__el__fech__dia ff--special ff--22">16</div>
+						<div class="bklast__age__el__fech__mes ff--special ff--18 cc--amarillo2 ff--r">Mar</div>
+						<div class="bklast__age__el__fech__hor ff--b ff--18">02:00</div>
+					</div>
+					<a class="bklast__age__el__text ff--16 ff--special" href="#">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</a>
+					<div class="bklast__age__el__place ff--14 ff--r ff--b cc--azul3">
+						Biblioteca central de la UNPRG
+					</div>
+					<div class="block__clean"></div>
+				</div>
+
+				<div class="bklast__age__el">
+					<div class="bklast__age__el__fech bgc--azul2 cc ff ff--c">
+						<div class="bklast__age__el__fech__dia ff--special ff--22">16</div>
+						<div class="bklast__age__el__fech__mes ff--special ff--18 cc--amarillo2 ff--r">Mar</div>
+						<div class="bklast__age__el__fech__hor ff--b ff--18">02:00</div>
+					</div>
+					<a class="bklast__age__el__text ff--16 ff--special" href="#">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					</a>
+					<div class="bklast__age__el__place ff--14 ff--r ff--b cc--azul3">
+						Biblioteca central de la UNPRG
+					</div>
+					<div class="block__clean"></div>
+				</div>
+
+			</div>
+		</div>
+	
+		<div class="block__clean"></div>
+	</div>
+</section>
