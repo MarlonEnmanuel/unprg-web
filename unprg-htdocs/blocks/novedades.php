81349cbab72598c24edec15ddf6fc26f91377adb
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2016-04-07 21:10:30 -0500

    Visor terminado

--- unprg-htdocs/blocks/novedades.php
+++ unprg-htdocs/blocks/novedades.php
@@ -1,72 +1,64 @@
+<div class="block bkvis">
+	<div class="bkvis__wraper">
+		<span class="bkvis__close icon-cancel-circle cc--gris5 ff--34"></span>
+		<div class="bkvis__wraper__cont">
+			<script type="text/template" id="template_visor_img">
+				<div class="bkvis__img bkvis__el">
+					<div class="bkvis__img__cell">
+						<div class="bkvis__img__cell__in">
+							<img src="<%= link %>" alt="" class="bkvis__img__cell__in__file">
+						</div>
+					</div>
+				</div>
+			</script>
+			<script type="text/template" id="template_visor_doc">
+				<div class="bkvis__doc bkvis__el">
+					<embed class="bkvis__doc__file" type="application/pdf" src="<%= link %>" />
+				</div>
+			</script>
+		</div>
+	</div>
+</div>
 <section class="block plx bklast bgc--gris6 cc--gris1">
 	<div class="block__wraper">
-
 		<div class="bklast__col bgc bklast__avi plx__item">
 			<div class="bklast__col__titulo ff--26 ff--special cc--azul2">Avisos</div>
 			<div class="bklast__col__cont bklast__avi__cont">
-
-				<script type="text/template" id="template_aviso" data-class="bklast__avi__el" data-tag="div">
-					<a class="bklast__avi__el__text ff--14" href="#">
-						<%= titulo %>
-					</a>
-					<div class="bklast__avi__el__fech ff--12 ff--r ff--b cc--azul2">
-						<%= fchReg %>
-					</div>
+				<script type="text/template" id="template_aviso">
+					<a class="bklast__avi__el__text ff--14" href="#"><%= titulo %></a>
+					<div class="bklast__avi__el__fech ff--12 ff--r ff--b cc--azul2"><%= fchReg %></div>
 				</script>
-
-				<div class="bklast__error ff--14 ff--c cc--gris1">
-					No se pudo obtener contenido :(
-				</div>
+				<div class="bklast__error ff--14 ff--c cc--gris1">No se pudo obtener contenido :(</div>
 			</div>
 		</div>
-
 		<div class="bklast__col bgc bklast__doc plx__item">
 			<div class="bklast__col__titulo ff--26 ff--special cc--azul2">Documentos</div>
 			<div class="bklast__col__cont bklast__doc__cont">
 				<script type="text/template" id="template_documento">
-					<div class="bklast__doc__el">
-						<span class="icon-file-pdf ff--44 cc--gris2"></span>
-						<a class="bklast__doc__el__text ff--14" href="#">
-							<%= nombre %>
-						</a>
-						<div class="bklast__doc__el__fech ff--12 ff--r ff--b cc--azul3">
-							<%= fchReg %>
-						</div>
-					</div>
+					<span class="icon-file-pdf ff--44 cc--gris2"></span>
+					<a class="bklast__doc__el__text ff--14" href="#"><%= nombre %></a>
+					<div class="bklast__doc__el__fech ff--12 ff--r ff--b cc--azul3"><%= fchReg %></div>
 				</script>
-				<div class="bklast__error ff--14 ff--c cc--gris1">
-					No se pudo obtener contenido :(
-				</div>
+				<div class="bklast__error ff--14 ff--c cc--gris1">No se pudo obtener contenido :(</div>
 			</div>
 		</div>
-
 		<div class="bklast__col bgc bklast__age plx__item">
 			<div class="bklast__col__titulo ff--26 ff--special cc--azul2">Agenda</div>
 			<div class="bklast__col__cont bklast__age__cont">
 				<script type="text/template" id="template_agenda">
-					<div class="bklast__age__el">
-						<div class="bklast__age__el__fech bgc--azul2 cc ff ff--c">
-							<div class="bklast__age__el__fech__dia ff--special ff--22"><%= fchInicio_dia %></div>
-							<div class="bklast__age__el__fech__mes ff--special ff--18 cc--amarillo2 ff--r"><%= fchInicio_mes %></div>
-							<div class="bklast__age__el__fech__hor ff--b ff--18"><%= fchInicio_hora %></div>
-						</div>
-						<a class="bklast__age__el__text ff--16 ff--special" href="#">
-							<%= titulo %>
-						</a>
-						<div class="bklast__age__el__place ff--14 ff--r ff--b cc--azul3">
-							<%= lugar %>
-						</div>
-						<div class="block__clean"></div>
+					<div class="bklast__age__el__fech bgc--azul2 cc ff ff--c">
+						<div class="bklast__age__el__fech__dia ff--special ff--22"><%= fchInicio_dia %></div>
+						<div class="bklast__age__el__fech__mes ff--special ff--18 cc--amarillo2 ff--r"><%= fchInicio_mes %></div>
+						<div class="bklast__age__el__fech__hor ff--b ff--18"><%= fchInicio_hora %></div>
 					</div>
+					<a class="bklast__age__el__text ff--16 ff--special" href="#"><%= titulo %></a>
+					<div class="bklast__age__el__place ff--14 ff--r ff--b cc--azul3"><%= lugar %></div>
+					<div class="block__clean"></div>
 				</script>
-				<div class="bklast__error ff--14 ff--c cc--gris1">
-					No se pudo obtener contenido :(
-				</div>
+				<div class="bklast__error ff--14 ff--c cc--gris1">No se pudo obtener contenido :(</div>
 			</div>
 		</div>
-	
 		<div class="block__clean"></div>
 	</div>
 </section>
-
 <script type="text/javascript" src="/frontend/js/novedades.js"></script>
