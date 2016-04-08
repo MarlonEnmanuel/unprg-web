044e283b82677290abf29972d9e0a1687d87822f
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2016-04-08 17:39:19 -0500

    Cover arreglado

--- unprg-htdocs/blocks/cover.php
+++ unprg-htdocs/blocks/cover.php
@@ -1,25 +1,28 @@
-<link rel="stylesheet" href="/frontend/owl-carousel/owl.carousel.css">
+<?php
+	require_once $_SERVER['DOCUMENT_ROOT'].'/backend/controllers/Controller.php';
+	require_once $_SERVER['DOCUMENT_ROOT'].'/backend/models/Portada.php';
+
+	$Controller = new Controller();
+	$mysqli = $Controller->getMysqli();
+	$covers = new Portada($mysqli);
+	$covers = $covers->search(true);
+?>
 
 <div class="block bkcov bgc--azul1">
 	<div class="bkcov__sl owl-carousel owl-theme">
-		<script type="text/template" id="template_portada">
-			<div class="item bkcov__sl__item">
-				<img class="bkcov__sl__item__img" src="<%= ruta %>" alt="">
-				<div class="bkcov__sl__item__text ff--condensed">
-					<div class="block__wraper plx__item slow">
-						<p class="bkcov__sl__item__text__1 ff--34 ff--b"><%= titulo %></p>
-						<p class="bkcov__sl__item__text__2 ff--18"><%= descripcion %></p>
+		<?php foreach ($covers as $key => $cov) { ?>
+				<div class="item bkcov__sl__item">
+					<img class="bkcov__sl__item__img" src="<?= $cov->ruta ?>" alt="<?= $cov->titulo ?>">
+					<div class="bkcov__sl__item__text ff--condensed">
+						<div class="block__wraper plx__item slow">
+							<p class="bkcov__sl__item__text__1 ff--34 ff--b"><?= $cov->titulo ?></p>
+							<p class="bkcov__sl__item__text__2 ff--18"><?= $cov->descripcion ?></p>
+						</div>
 					</div>
 				</div>
-			</div>
-		</script>
-
+		<?php } ?>
 	</div>
 	<div class="bkcov__bu--prev ff" title="Anterior"><span class="icon-circle-left"></span></div>
 	<div class="bkcov__bu--next ff" title="Siguiente"><span class="icon-circle-right"></span></div>
 </div>
-
 <script src="/frontend/owl-carousel/owl.carousel.min.js"></script>
-<script type="text/javascript" src="/frontend/js/cover.js"></script>
-
-
